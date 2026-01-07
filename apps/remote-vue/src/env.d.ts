1953cd31319ecc41b1d39c793916c8563f724b98
Yu Ling Chen <yo0.guitar.it@gmail.com>
2026-01-07 20:51:25 +0800

    新增 ESLint 配置，更新 package.json 以包含 lint 和格式化腳本，並新增相關的開發依賴

--- apps/remote-vue/src/env.d.ts
+++ apps/remote-vue/src/env.d.ts
@@ -3,7 +3,7 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
 
-  // biome-ignore lint/complexity/noBannedTypes: reason
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
